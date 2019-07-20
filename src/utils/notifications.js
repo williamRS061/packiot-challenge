30596061b9387886ccfe1d7df4046806421764b1
= <=>
2019-07-19 23:57:04 -0300

    remove unused notifications

--- src/utils/notifications.js
+++ src/utils/notifications.js
@@ -9,21 +9,6 @@ const toastConfig = {
   draggable: true
 };
 
-export const NotificationSuccess = msg => {
-  toast.success(msg, toastConfig);
-};
-
 export const NotificationError = msg => {
   toast.error(msg, toastConfig);
-};
-
-export const NotificationDefault = msg => {
-  toast(msg, {
-    position: "top-center",
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true
-  });
 };
